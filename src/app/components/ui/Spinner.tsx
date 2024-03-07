e8b6ffda47e46fe6eaa009771b925a97023dedf0
sanghyeon <uphyeon@kakao.com>
2024-03-07 11:23:44 +0900

    :lipstick: Spinner 컴포넌트 작성

---
+++ src/app/components/ui/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Spinner = () => {
+  return <div className="w-16 h-16 border-8 border-b-blue-500 rounded-full box-border animate-spin"></div>;
+};
+
+export default Spinner;
