abb79c7db6dbd002b16946da59cc081772830e6a
sanghyeon <uphyeon@kakao.com>
2024-03-08 00:18:50 +0900

    :sparkles: 상세 페이지 태그된 강의 링크 이동 구현

--- src/app/components/wiki/detail/WikiDetailDescription.tsx
+++ src/app/components/wiki/detail/WikiDetailDescription.tsx
@@ -1,7 +1,21 @@
+'use client';
+
 import React from 'react';
+import parse from 'html-react-parser';
+import { useRouter } from 'next/navigation';
 
 const WikiDetailDescription = ({ description }: { description: string }) => {
-  return <p>{description}</p>;
+  const router = useRouter();
+
+  const handleClick = (e: React.MouseEvent<HTMLParagraphElement>) => {
+    const target = e.target as HTMLElement;
+    if (target.tagName === 'BUTTON') {
+      const id = target.id.replace('link-', '');
+      router.push(`/detail/${id}`);
+    }
+  };
+
+  return <p onClick={handleClick}>{parse(`${description}`)}</p>;
 };
 
 export default WikiDetailDescription;
