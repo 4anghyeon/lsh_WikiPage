55339b8e78cad1d1c8275f9a5080722217471640
sanghyeon <uphyeon@kakao.com>
2024-03-08 23:17:34 +0900

    :lipstick: 메인, 상세 페이지 애니메이션 효과 추

--- src/app/components/wiki/detail/WikiDetail.tsx
+++ src/app/components/wiki/detail/WikiDetail.tsx
@@ -2,9 +2,18 @@ import React, { ReactNode } from 'react';
 import WikiDetailHeader from '@/app/components/wiki/detail/WikiDetailHeader';
 import WikiDetailTabHeader from '@/app/components/wiki/detail/WikiDetailTabHeader';
 import WikiDetailDescription from '@/app/components/wiki/detail/WikiDetailDescription';
+import { motion } from 'framer-motion';
 
 const WikiDetail = ({ children }: { children: ReactNode }) => {
-  return <section className="flex flex-col justify-between m-h-[384px] gap-5 ml-40 mr-40">{children}</section>;
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: -30 }}
+      animate={{ opacity: 1, y: 0, transition: { duration: 0.5 } }}
+      className="flex flex-col justify-between m-h-[384px] gap-5 ml-40 mr-40"
+    >
+      {children}
+    </motion.section>
+  );
 };
 
 export default WikiDetail;
--- src/app/components/wiki/list/WikiListRow.tsx
+++ src/app/components/wiki/list/WikiListRow.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
 import { WikiType } from '@/app/types/data';
+import { motion } from 'framer-motion';
 
 const WikiListRow = ({ data }: { data: WikiType }) => {
   return (
-    <Link
-      href={`/detail/${data.id}`}
-      className="border border-slate-200 rounded-lg bg-white p-5 hover:bg-slate-100 hover:duration-100 cursor-pointer"
-    >
-      {data.title}
+    <Link href={`/detail/${data.id}`}>
+      <motion.div
+        initial={{ opacity: 0, y: -30 }}
+        animate={{ opacity: 1, y: 0, transition: { duration: 0.5 } }}
+        className="border border-slate-200 rounded-lg bg-white p-5 hover:bg-slate-100 hover:duration-100 cursor-pointer"
+      >
+        {data.title}
+      </motion.div>
     </Link>
   );
 };
